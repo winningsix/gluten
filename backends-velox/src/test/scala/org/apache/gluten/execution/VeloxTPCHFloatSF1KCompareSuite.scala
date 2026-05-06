0219a9d03aabe8201355fb071c90e4ca3e98e21d
Ferdinand Xu <Ferdinandx@nvidia.com>
2026-05-06 12:38:50 +0000

    test: format SF1K property forwarding
    Apply the Spotless-required layout for the SF1K compare suite property forwarding block so the unified RC package can complete.

--- backends-velox/src/test/scala/org/apache/gluten/execution/VeloxTPCHFloatSF1KCompareSuite.scala
+++ backends-velox/src/test/scala/org/apache/gluten/execution/VeloxTPCHFloatSF1KCompareSuite.scala
@@ -142,11 +142,12 @@ class VeloxTPCHFloatSF1KCompareSuite extends VeloxTPCHTableSupport with TimeLimi
       "spark.driver.maxResultSize",
       "spark.gluten.sql.columnar.libpath",
       "spark.gluten.loadLibFromJar",
-      q4ExistsLineitemDedupKey).foreach { key =>
-      sys.props
-        .get(key)
-        .filter(value => value.nonEmpty && value != "null")
-        .foreach(conf.set(key, _))
+      q4ExistsLineitemDedupKey).foreach {
+      key =>
+        sys.props
+          .get(key)
+          .filter(value => value.nonEmpty && value != "null")
+          .foreach(conf.set(key, _))
     }
     conf
   }
